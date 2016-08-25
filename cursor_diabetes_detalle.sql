ab1616d6e39852dc26025aafdf64f89a775929fa
Xajil <xajil.developer@gmail.com>
2016-08-25 13:09:50 -0600

    En Pivote de Observaciones Campo Voided=0
    Se agregó el campo de voided=0 a las observaciones para emplear
    únicamente la observaciones válidas.

--- cursor_diabetes_detalle.sql
+++ cursor_diabetes_detalle.sql
@@ -1,64 +1,97 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `diabetes`()
 BEGIN
 
-  declare hemoglobina_id int default 159644;
-  declare peso_id int default 5089; 
+  
   declare sistolica_id int default 5085;
   declare diastolica_id int default 5086;
+  declare peso_id int default 5089;	
+  declare hemoglobina_id int default 159644;
+  declare talla_id int default 5090;
   
   declare var_person_id int;
   declare var_concept_id int;
   declare var_obs_date_created datetime;
-  declare var_value_numeric int;
+  declare var_value_numeric double;
   
+  declare var_tot_sis int default 0;
+  declare var_tot_peso int default 0;
+  declare var_tot_hemo int default 0;
+  declare var_tot_dias int default 0;
+
   declare tmp_person_id int default 0;
-  declare tmp_concept_id int default 0;
-  declare tmp_obs_date_created datetime;
-  declare tmp_value_numeric int;
+  
+  declare rec_person_id int; 
+  declare rec_numero_open int;
   
   DECLARE fin int default 0;
+
+  declare loop_peso int default 0;
+  
   declare obs_fin varchar(100);
   declare obs_fin_2 varchar(10000) default "";
+  
+  declare cont int default 0;
 
-  Declare var_cont_obs int; -- (select count(*) from obs_hemoglobina where person_id = 57 and concept_id = 159644 group by person_id, concept_id) ;
+  Declare var_cont_obs int; 
   
-  DECLARE cur_paciente cursor for select person_id, concept_id , date_created, value_numeric from tbl_obs_diabeticos where person_id = 57;-- CURSOR FOR SELECT id,data FROM test.t1;
-  
--- Declaración de un manejador de error tipo NOT FOUND
+  DECLARE cur_paciente cursor for select person_id , numero_open from tbl_diabeticos_prog ; 
+    
 DECLARE CONTINUE HANDLER FOR NOT FOUND SET fin = 1;
+
   
   OPEN cur_paciente;
 
   read_loop: LOOP
-    FETCH cur_paciente INTO var_person_id, var_concept_id ,var_obs_date_created, var_value_numeric;
+    FETCH cur_paciente INTO rec_person_id, rec_numero_open ;
 
   
     IF fin = 1 THEN
-    LEAVE read_loop;
-  END IF;    
+		LEAVE read_loop;
+	END IF;    
+    
 
-  set tmp_person_id = var_person_id;
-    set tmp_concept_id = var_concept_id;
-    set tmp_obs_date_created = var_obs_date_created;
-    set tmp_value_numeric = var_value_numeric;
+	BLOCK2:BEGIN
     
-    if (var_person_id = tmp_person_id and var_concept_id = hemoglobina_id ) then
-    set obs_fin = concat('|', tmp_value_numeric, '|',  convert( (date_format( tmp_obs_date_created,'%Y-%m-%d')), char), '|');
-    end if;    
+    DECLARE fin_obs int default 0;
     
-  if (var_person_id = tmp_person_id and var_concept_id = peso_id ) then
-    set obs_fin = concat('|', tmp_value_numeric, '|',  convert( (date_format( tmp_obs_date_created,'%Y-%m-%d')), char), '|');
-    end if;
+    declare cur_obs cursor for select person_id, concept_id, date_created, value_numeric from tbl_obs_diabeticos where person_id = rec_person_id 
+    and concept_id = talla_id;
     
-  set obs_fin_2 = concat(obs_fin_2, obs_fin);
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET fin_obs = 1;
+	
+		open cur_obs;
+		obs_loop: loop
+			fetch cur_obs into var_person_id, var_concept_id, var_obs_date_created, var_value_numeric; 
+			
+            if fin_obs =1 then
+				leave obs_loop;
+			end if;
+		    
+            set tmp_person_id = var_person_id;
+            
+                -- set obs_fin = concat( 'peso',loop_peso, '|' , DATE_FORMAT(var_obs_date_created,'%Y-%m-%d'),'|', var_value_numeric, '|');
+            	
+                set obs_fin = concat( DATE_FORMAT(var_obs_date_created,'%Y-%m-%d'),'|', var_value_numeric, '|');
+				
+                -- set obs_fin = concat(var_concept_id ,'|', var_person_id, '|');
+				
+                set obs_fin_2 = concat(obs_fin_2, obs_fin);
+                
+             -- insert into obs_horizontal ( person_id , obs) values (var_person_id, var_concept_id);
+                
+		end loop obs_loop;
+        
+		close cur_obs;
+        
+	END BLOCK2;
     
-  END LOOP read_loop;
-  
-    insert into obs_horizontal (person_id, obs) 
-    values (tmp_person_id, obs_fin_2);
-    -- values (tmp_person_id, concat(tmp_concept_id, '|',  convert( (date_format( tmp_date_obs,'%Y-%m-%d')), char) ) );
-   commit;
+
+		insert into obs_horizontal ( person_id , obs) values (rec_numero_open, obs_fin_2);
+		set obs_fin = "";
+		set obs_fin_2 = "";
+		
+    END LOOP read_loop;
 
   CLOSE cur_paciente;
-  commit;
+ 
 END
