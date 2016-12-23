32897b0a87e84c6718d40a3e7d374755a5ec78f8
Ikari <ikari@nef.lan>
2016-12-23 10:50:54 -0600

    Cambios en la forma de crear el pivote de nutrición. Se toman en cuentra programas específicos

--- PdA/fill_pda_pivot_report_help_to.sql
+++ PdA/fill_pda_pivot_report_help_to.sql
@@ -26,115 +26,24 @@ STR_TO_DATE('11/01/2016', '%m/%d/%Y'),
 STR_TO_DATE('11/30/2016', '%m/%d/%Y') 
 );
 
-select * from pda_pivot_report
-where pda_pivot_report.encounter_datetime  between STR_TO_DATE('11/01/2016', '%m/%d/%Y') and
-STR_TO_DATE('11/30/2016', '%m/%d/%Y');
 
-select * from pda_pivot_report;
+call proc_fill_from_encounter(
+STR_TO_DATE('12/01/2016', '%m/%d/%Y'), 
+STR_TO_DATE('12/30/2016', '%m/%d/%Y') 
+);
 
+
+/* 
 delete from  pda_pivot_report 
 where encounter_datetime between 
 	STR_TO_DATE('11/01/2016', '%m/%d/%Y') AND 
 	STR_TO_DATE('11/30/2016', '%m/%d/%Y'); 
 commit;
 
-/* delete from  pda_pivot_report where encounter_datetime between 
+delete from  pda_pivot_report where encounter_datetime between 
 	STR_TO_DATE('10/01/2016', '%m/%d/%Y') AND 
 	STR_TO_DATE('10/31/2016', '%m/%d/%Y'); 
 
 commit;
-
 */
-
-
--- 4570 
--- total de 730 encuentros.
--- total de 738 encuentro 
--- total de 739 encuentros 
--- total de 741 encuentros 20160921 
--- TOTAL DE 740 encuentros 
--- total de 746 encuentros 20160924
--- total de 749 encuentros 20161003
-
-
-
-select pda_pivot_report.* from pda_pivot_report;
-
-
-TRUNCATE TABLE error_log_pda_repo;
-
-select * from error_log_pda_repo;
-
-
---  TOTAL DE VISITAS A CADA PACIENTES
--- EXCLUYENDO LOS LABORATORIOS, CORRESPONDENCIAS 
--- pendiente los form_id 15 que provengan de provider_id 18.
-
-
---  TOTAL DE PACIENTES VISTOS CON ALGÚN SERVICIO EN EL MES
---  596
-select count(distinct( encounter_datetime ) ), patient_id, numero_open from pda_pivot_report
--- where patient_id = 1926
--- WHERE form_id NOT IN ( 12, 14) 
--- and provider_id <> 18
-group by patient_id, numero_open
--- having count(mes) > 2
-;
-
-
--- 557 
---  TOTAL DE PACIENTES VISTOS CON VISITAS DE ENFERMERAS O DR.
-select count(distinct( encounter_datetime ) ), patient_id, numero_open from pda_pivot_report
--- where patient_id = 1926
-WHERE form_id NOT IN ( 12, 14) 
-and provider_id <> 18
-group by patient_id, numero_open
--- having count(mes) > 2
-;
-
---  TOTAL DE ENCUENTROS O VISITAS EN EL MES 
-select distinct( encounter_datetime ), patient_id, numero_open from pda_pivot_report
-WHERE form_id NOT IN ( 12, 14)
-and provider_id <> 18
-group by patient_id, pda_pivot_report.encounter_datetime
-;
-
-
---  TOTAL DE ENCUENTROS O VISITAS EN EL MES 
--- que fueron reconsultas
-select distinct( encounter_datetime ), patient_id, numero_open from pda_pivot_report
-WHERE form_id NOT IN ( 12, 14)
-and provider_id <> 18
-and pda_pivot_report.tipo_consulta = 2
-group by patient_id, pda_pivot_report.encounter_datetime
-;
-
---  TOTAL DE ENCUENTROS O VISITAS EN EL MES 
--- que fueron primera_visita
-select distinct( encounter_datetime ), patient_id, numero_open from pda_pivot_report
-WHERE form_id NOT IN ( 12, 14)
-and provider_id <> 18
-and pda_pivot_report.tipo_consulta = 1
-group by patient_id, pda_pivot_report.encounter_datetime
-;
-
-select value_coded, obs.*
- from obs
-where concept_id = 1619 -- EXAMENES DE ETS EN MUJERES EMBARAZADA
-and obs.obs_datetime between  STR_TO_DATE('08/01/2016', '%m/%d/%Y') and STR_TO_DATE('08/31/2016', '%m/%d/%Y')
-and  voided=0
--- and value_coded = 1267
--- group by value_coded
-;
-
-
-select value_coded 
--- into var_sifilis 
-from obs, encounter
-where encounter.encounter_id = obs.encounter_id
-and obs.encounter_id = encounter.encounter_id
--- and obs.person_id = var_patient_id
-and concept_id = 1619 -- EXAMENES DE Sífilis
-and obs.obs_datetime between  STR_TO_DATE('08/01/2016', '%m/%d/%Y') and STR_TO_DATE('08/31/2016', '%m/%d/%Y') 
-and obs.voided = 0;
 
